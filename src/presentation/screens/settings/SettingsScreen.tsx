0257a2d8a2db6d68bd3fbc37623ca056165ac3f0
Miguel López <miguello@titamedia.com>
2024-04-06 16:18:28 -0500

    Refactor SettingsScreen component to use React Navigation and add navigation buttons

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,20 @@
+import {StackActions, useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {Text, View} from 'react-native';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {globalStyles} from '../../theme/theme';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation();
+
   return (
-    <View>
+    <View style={globalStyles.container}>
       <Text>SettingsScreen</Text>
+      <PrimaryButton label="Go to Back" onPress={() => navigator.goBack()} />
+      <PrimaryButton
+        label="Go to Home"
+        onPress={() => navigator.dispatch(StackActions.popToTop())}
+      />
     </View>
   );
 };
